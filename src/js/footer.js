421b79fd5ca88843c7b0388b33267d09d59cb995
GeorgeZJChen <georgechenzj@outlook.com>
2018-09-01 17:50:12 +0100

    go

--- src/js/footer.js
+++ src/js/footer.js
@@ -7,7 +7,7 @@ class Footer extends Component {
     return (
       <div className='footer'>
         <hr/>
-        <p className='footer-text'>Welcome to this site! There are more interesting projects presented</p>
+        <p className='footer-text'>Welcome to this site! There are more interesting projects presented.</p>
         <div className='footer-mp' onClick={()=>this.moreProjects()}>{"See more projects >"}</div>
         <h3>Contact:</h3>
         <p className='footer-text'>By email: <a className='footer-email' href="mailto:georgechenzj@outlook.com">georgechenzj@outlook.com</a></p>
